5c0017712ada8d3e2d79dce85075bfaf95abba6b
Gert Kanter <gert.kanter@ttu.ee>
2019-10-08 13:48:11 +0300

    Add resilience to stop

--- stop.py
+++ stop.py
@@ -1,5 +1,13 @@
 from PiBot import PiBot
 
+import time
+
 if __name__ == "__main__":
-    robot = PiBot()
-    robot.set_wheels_speed(0)
+    while True:
+        try:
+            robot = PiBot()
+            robot.set_wheels_speed(0)
+            break
+        except Exception as e:
+            time.sleep(0.05)
+            continue
